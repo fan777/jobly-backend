ab5a64921b3843b053596f6df735c89897d97388
fan777 <fan777@gmail.com>
2021-10-09 11:35:05 -0700

    gitignore + update db connection

--- db.js
+++ db.js
@@ -3,18 +3,26 @@
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
+let connParams = {
+  user: process.env.user,
+  password: process.env.password,
+  database: getDatabaseUri(),
+}
+
 let db;
 
 if (process.env.NODE_ENV === "production") {
   db = new Client({
-    connectionString: getDatabaseUri(),
+    // connectionString: getDatabaseUri(),
+    ...connParams,
     ssl: {
       rejectUnauthorized: false
     }
   });
 } else {
   db = new Client({
-    connectionString: getDatabaseUri()
+    // connectionString: getDatabaseUri()
+    ...connParams
   });
 }
 
